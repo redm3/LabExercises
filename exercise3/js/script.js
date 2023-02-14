453f7c23af2aba53ce4a8fa9a96eb1ffeb48be3c
marco <macmwz20@gmail.com>
2023-02-14 17:14:32 +1300

    dice thing

--- exercise3/js/script.js
+++ exercise3/js/script.js
@@ -15,3 +15,16 @@ function prize(){
 function changeParagraph(){
     document.getElementById('demo').innerHTML = 'NEW <strong>paragraph</strong> text';
 }
+
+function rollDice() {
+    var diceType = document.getElementById("dice-type").value;
+    var result = Math.floor(Math.random() * diceType) + 1;
+    document.getElementById("result").innerHTML = "Result: " + result;
+    if  (result == "6"){
+        alert(' CONGRATS YOU HAVE WON THE PRIZE CLICK ON PRIZE 2 TO SEE WHAT YOU WON')
+    }
+    else {alert('YOU HAVE LOST')}
+  }
+
+  function rolledAsix() {
+  }
