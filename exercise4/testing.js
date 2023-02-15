41010b91fd689a365b707303da24c91a1a3a5c63
marco <macmwz20@gmail.com>
2023-02-15 13:38:14 +1300

    testing

---
+++ exercise4/testing.js
@@ -0,0 +1,19 @@
+//Design a function which adds 2 numbers
+
+function sum(a,b){
+    return a+b
+
+}
+
+//List all of the possible “bad conditions” that will make the test fail
+
+if(sum(2,4)!=6){
+    throw new Error("error");
+}
+
+//needs to accept numbers so will fail with strings
+//only needs 2 inputs any more number inputs will fail
+//
+
+
+   
---
+++ exercise4/variables.js
@@ -0,0 +1,11 @@
+//create array that has 5 elements
+
+const Array = [1,2,3,4,5];
+
+//replace the valuse of 1 & 4
+
+Array[1] = 0;
+Array[4]= 0;
+
+//print 
+console.log(Array);
